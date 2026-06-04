53aaf9d78e4b4335de6f6eabf30aed7aed7b7833
Du Guangno <guangbosir@126.com>
2026-06-04 18:33:54 +0800

    完善了脚手架

---
+++ src/com/shiyan4/service/Main.java
@@ -0,0 +1,12 @@
+package com.shiyan4.service;
+
+/**
+ * @Author 杜光博
+ * @Date 2026-06-04 18:31
+ * @PackageName:com.shiyan4.entity
+ * @ClassName: Main
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class Main {
+}
